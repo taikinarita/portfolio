e3a0be83154477887a743d56d01c0371c1d6fca8
taikinarita <taiki.narita.k11ta@gmail.com>
2022-08-31 11:21:07 +0900

    layouts.blade.php & index.blade.php 作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts')
+@section('title', 'トップページ')
+@section('content')
+<div class="top-view">
+    <h1>新しい旅を見つけよう！</h1>
+    ***検索バー
+</div>
+<div class="top-new">
+    <h2>新着記事</h2>
+    ***新着記事
+    ***新着記事２
+    ***新着記事３
+    <a href="">新着記事一覧へ</a>
+</div>
+@endsection
+
---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <div class="header-wrapper">
+            <h1><a href="index.php" class="header-logo">＊＊＊仮ロゴ</a></h1>
+            <ul class="header-right">
+                <li><a href="">旅記録を見る</a></li>
+                <li><a href="">旅記録を書く</a></li>
+                <li><a href="">ログイン</a></li>
+                <li><a href="">新規登録</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="content">
+        @yield('content')
+    </div>
+    <footer>
+        <ul>
+            <li><a href="">旅記録を見る</a></li>
+            <li><a href="">旅記録を書く</a></li>
+            <li><a href="">本サイトの作成者について</a></li>
+            <li><a href="">お問い合わせ</a></li>
+        </ul>
+        <p class="copyright">©2022 Taiki Narita</p>
+    </footer>
+</body>
+</html>
